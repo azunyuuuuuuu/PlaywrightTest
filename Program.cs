0b7de03728b91019d258971cf2c3112438002184
Alice Triebull <azunyuuuuuuu@users.noreply.github.com>
2024-03-23 22:26:09 +0100

    simple playwright test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Playwright;
+
 Console.WriteLine("Hello, World!");
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+
+await page.ScreenshotAsync(new()
+{
+    Path = "screenshot.png"
+});
+await page.PdfAsync(new()
+{
+    Path = "printed.pdf",
+    Format = "A4"
+});
